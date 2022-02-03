d3ac04b61921a0ce0cde84640cfdb084f514bf51
Sergey Azarko <sokraza73@gmail.com>
2022-02-03 10:07:38 +0300

    4 commit

--- git_prob.py
+++ git_prob.py
@@ -1,5 +1,11 @@
-a = 'Hello'
-print(a)
-print(1 + 2)
-print(23 ** 2)
-print(123)
+a = int(input('Введите сумму вклада:  '))
+b = int(input('Введите процентную ставку:  '))
+s = int(input('Введите количество недель:  '))
+y = 100
+g = b / y
+count = a
+for i in range(s):
+    count += count * g
+print('Сумма за', s, 'недель(ли) составляет___', count)
+
+
